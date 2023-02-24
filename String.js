f70944e7f1f86e9e62fedae73151dd7502293feb
MartinMurithi <wachiramartin442@gmail.com>
2023-02-24 11:05:26 +0300

    Assessment

--- String.js
+++ String.js
@@ -5,23 +5,12 @@
 // When S = ' Tempor ip' the output should be ' pmeTi rop'
 
 function reverseGroupsOfFour(str) {
-  // Initialize an empty string to hold the transformed string
-  let transformedString = "";
+  let newStr = "";
 
-  // Loop through the string S in increments of 4 characters
-  for (let i = 0; i < str.length; i += 4) {
-    // Get the substring of 4 characters starting at the current index
-    const substring = str.substr(i, 4);
 
-    // Reverse the substring using the split(), reverse(), and join() methods
-    const reversedSubstring = substring.split("").reverse();
-
-    // Add the reversed substring to the transformed string
-    transformedString += reversedSubstring;
-  }
 
   // Return the transformed string
-  return transformedString;
+  return newStr;
 }
 
 console.log(reverseGroupsOfFour("martin"));
